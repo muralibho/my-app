9b9e13f65d6c358cf67f5fb2c23acee4da979ffe
muralibho <murali.ang2018@gmail.com>
2022-10-31 09:42:11 +0530

    my-app commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { CarsComponent } from './cars/cars.component';
 import { CartComponent } from './cart/cart.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
+import { CreateVehicleComponent } from './create-vehicle/create-vehicle.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmployeeRegistrationComponent } from './employee-registration/employee-registration.component';
 import { EventbindingComponent } from './eventbinding/eventbinding.component';
@@ -58,7 +59,7 @@ const routes: Routes = [
                           {path:'bank-details/:id', component:BankDetailsComponent},
                           {path:'vehicle-details/:id', component:VehicleDetailsComponent},
                           {path:'create-account', component:CreateAccountComponent, canDeactivate:[NotifyGuard]},
-                          {path:'create-vehicle', component:VehicleComponent}
+                          {path:'create-vehicle', component:CreateVehicleComponent}
                           
                         ]},
                         {path:'',component:LoginComponent},
